e956b8573920ed72358246e6702e4c6f32b14a80
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-21 19:12:55 -0300

    Adicionando arquivos para Deploy

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors())
+app.use(express.static('public'))
 app.use(express.json());
 
 const rotaUsuarios = require('./usuarios');
 app.use('/usuarios', rotaUsuarios);
 
-app.listen(3000);
+app.listen(PORT);
