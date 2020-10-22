ec99685ee6bc5ea579c60de9d4975d48f0412f1e
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-21 21:31:35 -0300

    Corrigindo erro de ENV

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ require('dotenv/config');
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PWD, {
     host: process.env.HOST,
     dialect: 'mysql',
+    port: 3306,
     define: {
         timestamps: false
     }
