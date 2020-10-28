c866d71c0283fa42e9e0ddfd78539125b35dd98a
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-28 20:38:54 -0300

    Consertando conexão com banco

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv/config');
 
-const sequelize = new Sequelize("climb_app", "climb_app", "n4*vl5cPo_", {
-    host: "climb_app.mysql.dbaas.com.br",
+const sequelize = new Sequelize(process.ENV.DATABASE, process.ENV.USER, process.ENV.PWD, {
+    host: process.ENV.HOST,
     dialect: 'mysql',
     port: 3306,
     define: {
