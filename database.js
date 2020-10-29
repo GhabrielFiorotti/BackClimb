947ea09a5fa0725fbb9752974533147a9206afa1
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-10-28 21:16:11 -0300

    Consertando conexão com banco

--- database.js
+++ database.js
@@ -1,18 +1,24 @@
 const Sequelize = require('sequelize');
 require('dotenv/config');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PWD, {
+    host: process.env.HOST,
+    dialect: 'mysql',
     define: {
         timestamps: false
     }
 });
-
-//host: process.env.HOST,
-//dialect: 'mysql',
 console.log(process.env.DATABASE);
 console.log(process.env.HOST);
 console.log(process.env.PWD);
 console.log(process.env.USER);
+
+/* const sequelize = new Sequelize(process.env.DATABASE_URL,{
+    define: {
+        timestamps: false
+    }
+});
+ */
 module.exports = {
     Sequelize: Sequelize,
     sequelize: sequelize
