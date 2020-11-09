a8e1e28f25b773c2c6d73ffbdd1139754950631b
Ghabriel Schiffelbein Fiorotti <ghabriel@ucl.br>
2020-11-09 20:48:09 -0300

    Adicionando rota Wallet

--- model/Wallet.js
+++ model/Wallet.js
@@ -3,14 +3,7 @@ const database = require('../database.js');
 
 const wallets = database.sequelize.define('wallets', {
     wallet_id : {type: database.Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
-    user_id: {
-        type: database.Sequelize.INTEGER,
-        allowNull : false,
-        references: {
-            model: users,
-            key: 'user_id'
-        }
-    },
+    user_id: {type: database.Sequelize.INTEGER, allowNull : false},
     name : {type : database.Sequelize.STRING(30)},
     description : {type : database.Sequelize.STRING(100)}
 }); 
